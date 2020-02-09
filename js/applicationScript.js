d72bd01b37be40da00d423d53142e39e60ac5cf0
rwth-enes <mail@rwth>
2020-02-09 00:05:36 +0000

    ApplicationScript edited by userTitle

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -42,7 +42,7 @@ var init = function() {
 
   client = new Las2peerWidgetLibrary("$Microservice_Url$/test13", iwcCallback); 
  
-        console.log("test1234");
+        console.log("test12345");
 
 
 
